f80a78149888583d0bd23cfad17f937644837135
Eric Vinicius Basso <eric.viniciusbasso@gmail.com>
2024-05-29 16:01:38 -0300

    ajustando para semi-standalone

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
 
 
-bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
-})
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
